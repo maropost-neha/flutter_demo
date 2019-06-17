4e9138e0a4af60cd1f0a8dbd01a847d9bcfbe152
maropost-neha <neha.kareer@maropost.com>
2019-06-17 12:35:30 +0530

    maps feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,54 @@
-import 'package:flutter/material.dart';
-import 'app_screens/note_list.dart';
+import 'dart:async';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'app_screens/anim_one.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Animation'),
+          ),
+          body: AnimOne(),
+        ));
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> markers = Set();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(21.7679, 78.8718),
+    zoom: 4.0,
+  );
 
   @override
   Widget build(BuildContext context) {
+    markers.add(
+      Marker(markerId: MarkerId('value'), position: LatLng(21.7679, 78.8718)),
+    );
 
-    return MaterialApp(
-      title: 'NoteKeeper',
-      debugShowCheckedModeBanner: false,
-      home: NoteList(),
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _kGooglePlex,
+      markers: markers,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
     );
   }
 }
